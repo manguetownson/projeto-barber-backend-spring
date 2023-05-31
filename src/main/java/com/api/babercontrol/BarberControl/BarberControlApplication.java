a67b73c74a13de0905f2f2115eb8b10954e35e04
manguetownson <luna.artur1999@gmail.com>
2023-05-31 02:42:50 -0300

    subindo o projeto

---
+++ src/main/java/com/api/babercontrol/BarberControl/BarberControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.babercontrol.BarberControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/babercontrol/BarberControl/BarberControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.babercontrol.BarberControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
