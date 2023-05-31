302eb0b6b817887b20e638a90c3067a5e526da06
manguetownson <luna.artur1999@gmail.com>
2023-05-31 16:59:03 -0300

    model fornecedor

---
+++ src/main/java/com/api/babercontrol/BarberControl/models/Fornecedor.java
@@ -0,0 +1,19 @@
+package com.api.babercontrol.BarberControl.models;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="fornecedor")
+@Getter
+@Setter
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long nome;
+    private Long produto;
+}
