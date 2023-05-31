0e27630ed38aca4ecbdea042c5a5c2ed5eebf536
manguetownson <luna.artur1999@gmail.com>
2023-05-31 16:47:11 -0300

    cliente herda usuario

--- src/main/java/com/api/babercontrol/BarberControl/models/Cliente.java
+++ src/main/java/com/api/babercontrol/BarberControl/models/Cliente.java
@@ -2,68 +2,15 @@ package com.api.babercontrol.BarberControl.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name="cliente")
 @Getter
 @Setter
-public class Cliente implements UserDetails {
-    private static final long serialVersionUID = 1L;
+public class Cliente extends Usuario {
 
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String username;
-
-    private String password;
-
-    private boolean enabled;
-
-    private String firstName;
-
-    private String lastName;
-
-    private String email;
-
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return authorities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
 }
