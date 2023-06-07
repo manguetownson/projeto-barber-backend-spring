3220d6b5996d446f9ba2190292fde13cb3026f4b
manguetownson <luna.artur1999@gmail.com>
2023-06-07 00:03:06 -0300

    usuario repository

--- src/main/java/com/api/babercontrol/BarberControl/repositories/UsuarioRepository.java
+++ src/main/java/com/api/babercontrol/BarberControl/repositories/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package com.api.babercontrol.BarberControl.repositories;
 
-public interface UsuarioRepository {
+import com.api.babercontrol.BarberControl.models.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 }
