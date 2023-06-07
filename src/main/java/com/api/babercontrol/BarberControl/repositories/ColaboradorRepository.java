a988ecd43640f4ce483b2feb4cdcf5c5d2b3f3f7
manguetownson <luna.artur1999@gmail.com>
2023-06-07 00:02:03 -0300

    colaborador repository

---
+++ src/main/java/com/api/babercontrol/BarberControl/repositories/ColaboradorRepository.java
@@ -0,0 +1,7 @@
+package com.api.babercontrol.BarberControl.repositories;
+
+import com.api.babercontrol.BarberControl.models.Colaborador;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ColaboradorRepository extends CrudRepository<Colaborador, Long> {
+}
