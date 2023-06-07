893657931ae772db31c3d0972e7e1b7701231834
manguetownson <luna.artur1999@gmail.com>
2023-06-06 23:57:59 -0300

    cliente service

--- src/main/java/com/api/babercontrol/BarberControl/services/ClienteService.java
+++ src/main/java/com/api/babercontrol/BarberControl/services/ClienteService.java
@@ -1,4 +1,9 @@
 package com.api.babercontrol.BarberControl.services;
 
-public class ClienteService {
+import com.api.babercontrol.BarberControl.models.Cliente;
+
+import java.util.List;
+
+public interface ClienteService {
+    List<Cliente> findAll();
 }
---
+++ src/main/java/com/api/babercontrol/BarberControl/services/ClienteServiceImpl.java
@@ -0,0 +1,24 @@
+package com.api.babercontrol.BarberControl.services;
+
+
+import com.api.babercontrol.BarberControl.models.Cliente;
+import com.api.babercontrol.BarberControl.repositories.ClienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements  ClienteService{
+
+    private ClienteRepository repository;
+
+
+    @Override
+    @Transactional
+
+    public List<Cliente> findAll() {
+
+        return (List<Cliente>) repository.findAll();
+    }
+}
