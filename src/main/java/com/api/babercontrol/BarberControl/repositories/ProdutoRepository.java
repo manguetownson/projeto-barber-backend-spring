0f1fb3e920dc157bf9248bfb3005bd74fba2b666
manguetownson <luna.artur1999@gmail.com>
2023-06-07 00:02:32 -0300

    produto repository

--- src/main/java/com/api/babercontrol/BarberControl/repositories/ProdutoRepository.java
+++ src/main/java/com/api/babercontrol/BarberControl/repositories/ProdutoRepository.java
@@ -1,4 +1,7 @@
 package com.api.babercontrol.BarberControl.repositories;
 
-public interface ProdutoRepository {
+import com.api.babercontrol.BarberControl.models.Produto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Long> {
 }
