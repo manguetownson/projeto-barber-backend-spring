4cf375bda2e7f87851bcc4c29060f19675887c2c
manguetownson <luna.artur1999@gmail.com>
2023-06-07 00:02:18 -0300

    fornecedor repository

---
+++ src/main/java/com/api/babercontrol/BarberControl/repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.api.babercontrol.BarberControl.repositories;
+
+import com.api.babercontrol.BarberControl.models.Fornecedor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FornecedorRepository extends CrudRepository<Fornecedor, Long> {
+}
