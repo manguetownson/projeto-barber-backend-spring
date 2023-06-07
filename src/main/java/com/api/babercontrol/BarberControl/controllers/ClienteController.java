11b62d1cee1c9b3c0dac023715763074baf48b08
manguetownson <luna.artur1999@gmail.com>
2023-06-07 00:03:24 -0300

    cliente controller

--- src/main/java/com/api/babercontrol/BarberControl/controllers/ClienteController.java
+++ src/main/java/com/api/babercontrol/BarberControl/controllers/ClienteController.java
@@ -1,4 +1,16 @@
 package com.api.babercontrol.BarberControl.controllers;
 
+import com.api.babercontrol.BarberControl.models.Cliente;
+import com.api.babercontrol.BarberControl.services.ClienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ClienteController {
+    private ClienteService service;
+
+    @GetMapping("/cliente")
+    public List<Cliente> list() { return service.findAll(); }
 }
