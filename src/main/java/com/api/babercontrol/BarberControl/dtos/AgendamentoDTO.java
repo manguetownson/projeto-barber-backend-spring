9e5e458edd4c7f334b9526224334b0bac4aeff7e
manguetownson <luna.artur1999@gmail.com>
2023-06-09 04:19:39 -0300

    DTO para agendamento

--- src/main/java/com/api/babercontrol/BarberControl/dtos/AgendamentoDTO.java
+++ src/main/java/com/api/babercontrol/BarberControl/dtos/AgendamentoDTO.java
@@ -1,4 +1,10 @@
 package com.api.babercontrol.BarberControl.dtos;
 
-public class AgendamentoDTO {
+
+//public record AgendamentoDTO(@NotBlank Long horario, @NotBlank Long data, @NotNull String servico) {
+//}
+
+import jakarta.validation.constraints.NotNull;
+
+public record AgendamentoDTO(@NotNull Long horario, @NotNull Long data, @NotNull String servico) {
 }
