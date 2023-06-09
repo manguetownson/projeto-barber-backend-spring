2db0e86c4c9db050fa626eb368ffd9b0863a53ce
manguetownson <luna.artur1999@gmail.com>
2023-06-09 04:22:02 -0300

    anotação de segurança na classe principal

--- src/main/java/com/api/babercontrol/BarberControl/BarberControlApplication.java
+++ src/main/java/com/api/babercontrol/BarberControl/BarberControlApplication.java
@@ -2,8 +2,9 @@ package com.api.babercontrol.BarberControl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BarberControlApplication {
 
 	public static void main(String[] args) {
