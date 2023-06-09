695fd1b7d3912b778cbaeae99b536d4cc13d85cb
manguetownson <luna.artur1999@gmail.com>
2023-06-09 04:20:14 -0300

    agendamento entity

--- src/main/java/com/api/babercontrol/BarberControl/models/Agendamento.java
+++ src/main/java/com/api/babercontrol/BarberControl/models/Agendamento.java
@@ -5,15 +5,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+import java.util.UUID;
+
 
 @Entity
 @Table(name="agendamento")
 @Getter
 @Setter
-public class Agendamento {
+public class Agendamento implements Serializable {
+    private static final long serialVersionUID = 1l;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id_agendamento;
     private Long horario;
     private Long data;
     private String servico;
