d23287cad81dd12ba67513dd080142ded9bbe0d6
manguetownson <luna.artur1999@gmail.com>
2023-06-09 06:03:17 -0300

    criação de id's para cliente e funcionario

--- src/main/java/com/api/babercontrol/BarberControl/models/Cliente.java
+++ src/main/java/com/api/babercontrol/BarberControl/models/Cliente.java
@@ -1,10 +1,13 @@
 package com.api.babercontrol.BarberControl.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 
 @Entity
@@ -13,4 +16,6 @@ import lombok.Setter;
 @Setter
 public class Cliente extends Usuario {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID idcliente;
 }
